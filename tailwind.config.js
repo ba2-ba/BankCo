b246842780fc55e0c39c5d030deabd30ae91f59e
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2024-04-11 21:43:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+
+    },
+
+  container:{
+    center:true,
+    padding: {
+      DEFAULT: '200px',
+      md: "50px"
+    }
+  }  
+  },
+  plugins: [],
+}
+
